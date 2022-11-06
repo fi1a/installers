1d8cf1baec9e176388767f5f1e050fd785d8a493
Nikolay Gorohovsky <fi1a@icloud.com>
2022-11-06 05:57:42 +0500

    Ввод вывод в классе библиотеки

--- src/AbstractLibrary.php
+++ src/AbstractLibrary.php
@@ -4,9 +4,30 @@ declare(strict_types=1);
 
 namespace Fi1a\Installers;
 
+use Fi1a\Console\IO\InputInterface;
+use Fi1a\Console\IO\OutputInterface;
+
 /**
  * Абстрактный класс библиотеки
  */
 abstract class AbstractLibrary implements LibraryInterface
 {
+    /**
+     * @var OutputInterface
+     */
+    protected $output;
+
+    /**
+     * @var InputInterface
+     */
+    protected $stream;
+
+    /**
+     * @inheritDoc
+     */
+    public function __construct(OutputInterface $output, InputInterface $stream)
+    {
+        $this->output = $output;
+        $this->stream = $stream;
+    }
 }
--- src/LibraryInterface.php
+++ src/LibraryInterface.php
@@ -4,11 +4,19 @@ declare(strict_types=1);
 
 namespace Fi1a\Installers;
 
+use Fi1a\Console\IO\InputInterface;
+use Fi1a\Console\IO\OutputInterface;
+
 /**
  * Интерфейс библиотеки
  */
 interface LibraryInterface
 {
+    /**
+     * Конструктор
+     */
+    public function __construct(OutputInterface $output, InputInterface $stream);
+
     /**
      * Можно установить библиотеку или нет
      */
